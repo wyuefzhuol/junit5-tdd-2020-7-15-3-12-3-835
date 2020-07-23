ae7171281055bfe423b351d7558f2568df69bbf5
ERIC WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGER3@OOCL.COM>
2020-07-23 12:29:09 +0800

    [TDD][Eric] feature: pass should_return_fizzbuzz_when_count_off_given_15

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -3,6 +3,9 @@ package junit5.tdd;
 public class FizzBuzz {
 
     public String play(int inputNumber) {
+        if (inputNumber % 3 == 0 && inputNumber % 5 == 0) {
+            return "FizzBuzz";
+        }
         if (inputNumber % 3 == 0) {
             return "Fizz";
         }
